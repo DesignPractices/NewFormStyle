aaf32d4d81303aa0d4e905d8f05e506ac9d08c58
vineeth1991 <vineeth1991@gmail.com>
2015-07-26 12:19:26 +0530

    final touch

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,16 @@
 $(function() {
   var $input = $(".text");
   $input.on("focusin", function() {
-    $(this).siblings("label").css({ "bottom": "50px", "font-size": "16px", "color": "#1fa7ba" })
+    $(this).css({ "border-bottom" : "2px solid #1fa7ba" });
+    $(this).siblings("label").css({ "bottom": "50px", "font-size": "16px", "color": "#1fa7ba" });
   });
   $input.on("focusout", function() {
     if(!$(this).val()) {
-      $(this).siblings("label").css({ "bottom": "5px", "font-size": "26px", "color": "#aaaaaa" })
+      $(this).css({ "border-bottom" : "2px solid #c4c4c4" });
+      $(this).siblings("label").css({ "bottom": "5px", "font-size": "26px", "color": "#aaaaaa" });
     } else {
-      $(this).siblings("label").css({ "bottom": "50px", "font-size": "16px", "color": "#1fa7ba" })
+      $(this).css({ "border-bottom" : "2px solid #1fa7ba" });
+      $(this).siblings("label").css({ "bottom": "50px", "font-size": "16px", "color": "#1fa7ba" });
     }
   });
 });
