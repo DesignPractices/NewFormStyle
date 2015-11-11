69c0f20cb318bfd8c0bd76558488044919395c9e
vineeth1991 <vineeth1991@gmail.com>
2015-11-11 11:42:48 +0530

    JS Changed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,41 @@
-$(function() {
-  var $input = $(".text");
-  $input.on("focusin", function() {
-    $(this).css({ "border-bottom" : "2px solid #1fa7ba" });
-    $(this).siblings("label").css({ "bottom": "50px", "font-size": "16px", "color": "#1fa7ba" });
-  });
-  $input.on("focusout", function() {
-    if(!$(this).val()) {
-      $(this).css({ "border-bottom" : "2px solid #c4c4c4" });
-      $(this).siblings("label").css({ "bottom": "5px", "font-size": "26px", "color": "#aaaaaa" });
-    } else {
+'use strict';
+
+var FormLabelModule = (function ($) {
+
+  $(function() {
+
+    var $input = $(".text");
+
+    function handleFocusIn() {
       $(this).css({ "border-bottom" : "2px solid #1fa7ba" });
-      $(this).siblings("label").css({ "bottom": "50px", "font-size": "16px", "color": "#1fa7ba" });
+      $(this).siblings("label").css({
+        "bottom": "50px",
+        "font-size": "16px",
+        "color": "#1fa7ba"
+      });
     }
-  });
-});
+
+    function handleFocusOut() {
+      if(!$(this).val()) {
+        $(this).css({ "border-bottom" : "2px solid #c4c4c4" });
+        $(this).siblings("label").css({
+          "bottom": "5px",
+          "font-size": "26px",
+          "color": "#aaaaaa"
+        });
+      } else {
+        $(this).css({ "border-bottom" : "2px solid #1fa7ba" });
+        $(this).siblings("label").css({
+          "bottom": "50px",
+          "font-size": "16px",
+          "color": "#1fa7ba"
+        });
+      }
+    }
+
+    $input.on("focusin", handleFocusIn);
+    $input.on("focusout", handleFocusOut);
+
+    });
+
+})(jQuery);
